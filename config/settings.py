78630675c73f86cfec22c715369453068cf0fea1
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-10-19 14:53:50 -0400

    trying to deploy to heroku

--- config/settings.py
+++ config/settings.py
@@ -22,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sxsc*&d)w0pw9os$)iaiq^-rq2mm8vjmgyd0-z^wk3r=24#b4q'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'django-fullstack-chatapp.herokuapp.com'
+]
 
 
 # Application definition
